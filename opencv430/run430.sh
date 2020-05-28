98b673e38c4e581f2b51136b9075c1527981fc2c
sowd0726 <sowd0726@gmail.com>
2020-05-28 18:54:35 +0900

    Changes tag to run from opencv430:102 to opencv430:104

--- opencv430/run430.sh
+++ opencv430/run430.sh
@@ -1,1 +1,1 @@
-sudo docker run -it --rm --net=host --runtime nvidia  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix opencv430:102
+sudo docker run -it --rm --net=host --runtime nvidia  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix opencv430:104
